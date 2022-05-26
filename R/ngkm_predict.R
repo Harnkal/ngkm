4b1d8e5da31e50508e267c5a5f730d6402ba51c7
Rafael da Rocha Silva <rrocha@altair.com>
2022-05-26 10:38:17 -0300

    updating documentation

--- R/ngkm_predict.R
+++ R/ngkm_predict.R
@@ -1,6 +1,6 @@
 #' ngkm_predict
 #'
-#' This function returns predictions give an input text and a model. The number 
+#' This function returns predictions given an input text and a model. The number
 #' of predictions will depend on the model
 #'
 #' @param input A string that will be used as predictor for the model.
@@ -9,22 +9,22 @@
 #' @export
 ngkm_predict <- function(input, model) {
     pred_tokens <- c(rep("<NA>", model$ngrams-1), ngkm_prep(input, model)[[1]])
-    
+
     pred_tokens <- tail(pred_tokens, model$ngrams-1)
-    
+
     search_fields <- paste("word_", (model$ngrams-1):1, sep = "")
-    
+
     pred <- model$pred_table
-    
+
     for (i in 1:(model$ngrams-1)) {
         pred <- pred[get(search_fields[i]) == pred_tokens[i]|get(search_fields[i])== "<NA>",]
     }
-    
+
     pred <- setorder(pred, -adjprob)
-    
+
     pred <- pred[, head(.SD, 1), by = "word"]
-    
+
     pred <- head(pred, model$npred)
-    
+
     return(pred)
 }
--- R/ngkm_train.R
+++ R/ngkm_train.R
@@ -2,41 +2,41 @@
 
 #' ngkm_train
 #'
-#' Trains an ngk model. 
+#' Trains an ngk model.
 #' Warning: can be very intense on your computer depending on the numbers you select.
 #'
 #' @param token_object A token object from quanteda to be transformed into a dfm.
-#' @param ngrams The maximum number of ngrams to used for training. It is 
-#' important to notice that if you train a model with ngrams = 5, the model will 
+#' @param ngrams The maximum number of ngrams to used for training. It is
+#' important to notice that if you train a model with ngrams = 5, the model will
 #' be created with 4 predictors and 1 predicter tokens.
-#' @param lambda The rate at which a prediction probability will be penalized for 
-#' having less than the maximum number of ngrams as predictors. The penalization 
+#' @param lambda The rate at which a prediction probability will be penalized for
+#' having less than the maximum number of ngrams as predictors. The penalization
 #' is exponential to the amount of missing matching tokens
-#' @param k Any ngram that appears k or less times in the dataset is going to be 
+#' @param k Any ngram that appears k or less times in the dataset is going to be
 #' removed from the model
 #' @param npred the number of prediction the model is going to return
-#' @param no_pred_list a vector containing words that might be used as a predictor, 
+#' @param no_pred_list a vector containing words that might be used as a predictor,
 #' but will never be predicted by the model
 #' @return A list of tokens
 #' @export
-ngkm_train <- function(token_object, ngrams = 2, lambda = 0.4, k = 0, npred = 3, 
+ngkm_train <- function(token_object, ngrams = 2, lambda = 0.4, k = 0, npred = 3,
     no_pred_list = c("<unk>", "<no>", "<url>")) {
-    
+
     ## Vars prep
     new.cols <- paste("word_", 0:(ngrams-1), sep="")
     new.cols[new.cols=="word_0"] <- "word"
     independent_cols <- rev(setdiff(new.cols, "word"))
-    
+
     ## Creating prediction table
     pred_table <- dfm(tokens_ngrams(token_object, n = 1:ngrams))
     pred_table <- featfreq(pred_table)
-    
+
     pred_table <- data.frame(pred_table)
     names(pred_table) <- "frequency"
     pred_table["full_token"] <- row.names(pred_table)
-    
+
     pred_table <- as.data.table(pred_table)
-    
+
     pred_table[, paste("V", 1:ngrams, sep="") := tstrsplit(full_token, "_", fixed = TRUE, fill = "<NA>")]
     pred_table[, full_token := NULL]
     pred_table[, ngram := rowSums(.SD != "<NA>"), .SDcol = 2:(ngrams+1)]
@@ -47,42 +47,38 @@ ngkm_train <- function(token_object, ngrams = 2, lambda = 0.4, k = 0, npred = 3,
         }
     }
     pred_table[, paste("V", 1:ngrams, sep="") := rep(NULL, ngrams)]
-    
+
     setcolorder(pred_table, append(rev(new.cols), "frequency"))
-    
-    
+
+
     pred_table[, prob := frequency/sum(frequency), by = independent_cols]
-    
+
     pred_table <- subset(pred_table, frequency > k)
     pred_table <- subset(pred_table, word %!in% no_pred_list)
     pred_table <- pred_table[, head(.SD, npred), by = independent_cols]
-    
+
     pred_table[, adjprob := prob * (lambda ^ (ngrams - ngram))]
-    
+
     setorder(pred_table, -adjprob)
-    
+
     ## additional info
     input_dictionary <- unique(unlist(pred_table[,0:(ngrams-1)]))
-    
+
     cutpoint <-  max(nchar(input_dictionary))*5*ngrams
-    
+
     ## assembling model
     model <- list(pred_table, input_dictionary, cutpoint, ngrams, lambda, k, npred)
     names(model) <-  c("pred_table", "input_dictionary", "cutpoint", "ngrams", "lambda", "k", "npred")
-    
+
     ## returning
     return(model)
 }
 
-#' A model trained using about 3.2 million documents
+#' ngkmodel
 #'
-#' The model was trained with ngrams = 5, lambda = 0.75, k = 1 an npred = 6.
+#' A model trained using about 3.2 million documents. The model was trained with
+#' ngrams = 5, lambda = 0.75, k = 1 an npred = 6.
 #'
-#' @format a ngkm model:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
+#' @format a ngkm model
 #' @source \url{https://github.com/Harnkal/DS_CapstoneProject}
 "ngkmodel"
